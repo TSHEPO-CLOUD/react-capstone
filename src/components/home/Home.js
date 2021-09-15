421cc20e8eea596b15c4a5134a192ad4560492bb
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-09-15 22:52:46 +0200

    Add home function

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,14 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { fetchCountries } from '../../redux/countries/countries';
 
-const Home = () => <Link to="/details/adfad">To Details</Link>;
 
-export default Home;
+const Home = () => {
+ const continent = 'Europe';
+
+ const dispatch = useDispatch();
+ const { list, total, loading } = useSelector((state) => ({
+   ...state.countries,
+   loading: state.loadingBar.default,
+ }));
