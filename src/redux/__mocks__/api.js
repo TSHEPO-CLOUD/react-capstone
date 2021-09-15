d2169041647ab4c7d9022136f4a0a7d8d60afcf2
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-09-15 22:39:51 +0200

    Add mocks

---
+++ src/redux/__mocks__/api.js
@@ -0,0 +1,14 @@
+export const getCountries = async () => Promise.resolve({
+ Testing1: {
+   All: {
+     confirmed: 5,
+     country: 'Testing1',
+   },
+ },
+ Testing2: {
+   All: {
+     confirmed: 8,
+     country: 'Testing2',
+   },
+ },
+});
