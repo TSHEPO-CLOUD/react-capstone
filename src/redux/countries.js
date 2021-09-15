9208f354e9859f1af8784260c4834e92d6fe862d
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-09-15 22:47:52 +0200

    Add test for countries

--- src/redux/countries.js
+++ src/redux/countries.js
@@ -8,3 +8,56 @@ export const loadCountries = (payload) => ({
   type: LOAD_COUNTRIES,
   payload,
 });
+
+export const loadCountry = (payload) => ({
+  type: LOAD_COUNTRY,
+  payload,
+});
+
+export const fetchCountry = (name) => async (dispatch) => {
+  dispatch(showLoading());
+  const data = await API.getCountry(name);
+
+  dispatch(loadCountry(data));
+  dispatch(hideLoading());
+};
+
+export const fetchCountries = (continent) => async (dispatch) => {
+  dispatch(showLoading());
+  const map = await API.getCountries(continent);
+
+  const data = Object.values(map).reduce((accumulator, currentValue) => {
+    const { All: country } = currentValue;
+    accumulator.list.push(country);
+    accumulator.total += country.confirmed;
+
+    return accumulator;
+  }, {
+    total: 0,
+    list: [],
+  });
+
+  data.list = data.list.sort((a, b) => b.confirmed - a.confirmed);
+
+  dispatch(loadCountries(data));
+  dispatch(hideLoading());
+};
+
+const initialState = {
+  total: 0,
+  list: [],
+  selected: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_COUNTRIES:
+      return action.payload;
+    case LOAD_COUNTRY:
+      return { ...state, selected: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
