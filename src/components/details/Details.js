5de67c676aeb14c31f134fb59738e57e38a889e4
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-09-15 22:51:46 +0200

    Refactore details

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -10,3 +10,24 @@ const Details = () => {
    loading: state.loadingBar.default,
    country: state.countries.selected,
  }));
+
+ useEffect(() => {
+  dispatch(fetchCountry(name));
+}, []);
+
+if (loading || !country) {
+  return <h1>Loading...</h1>;
+}
+
+const { All } = country;
+
+return (
+  <div>
+    <Link to="/">Go Back</Link>
+    <h1>{All.country}</h1>
+    <h1>{All.confirmed}</h1>
+  </div>
+);
+};
+
+export default Details;
