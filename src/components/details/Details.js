e6c33dc984d10493073fe86cf4346e56b4c1aa36
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-09-15 23:23:51 +0200

    Add jest

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,33 +1,33 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
-import { fetchCountry } from '../../redux/countries/countries';
+import { fetchCountry } from '../../redux/countries';
 
 const Details = () => {
- const { name } = useParams();
- const dispatch = useDispatch();
- const { country, loading } = useSelector((state) => ({
-   loading: state.loadingBar.default,
-   country: state.countries.selected,
- }));
+  const { name } = useParams();
+  const dispatch = useDispatch();
+  const { country, loading } = useSelector((state) => ({
+    loading: state.loadingBar.default,
+    country: state.countries.selected,
+  }));
 
- useEffect(() => {
-  dispatch(fetchCountry(name));
-}, []);
+  useEffect(() => {
+    dispatch(fetchCountry(name));
+  }, []);
 
-if (loading || !country) {
-  return <h1>Loading...</h1>;
-}
+  if (loading || !country) {
+    return <h1>Loading...</h1>;
+  }
 
-const { All } = country;
+  const { All } = country;
 
-return (
-  <div>
-    <Link to="/">Go Back</Link>
-    <h1>{All.country}</h1>
-    <h1>{All.confirmed}</h1>
-  </div>
-);
+  return (
+    <div>
+      <Link to="/">Go Back</Link>
+      <h1>{All.country}</h1>
+      <h1>{All.confirmed}</h1>
+    </div>
+  );
 };
 
 export default Details;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,52 +1,51 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchCountries } from '../../redux/countries/countries';
-
+import { fetchCountries } from '../../redux/countries';
 
 const Home = () => {
- const continent = 'Europe';
+  const continent = 'Europe';
 
- const dispatch = useDispatch();
- const { list, total, loading } = useSelector((state) => ({
-   ...state.countries,
-   loading: state.loadingBar.default,
- }));
+  const dispatch = useDispatch();
+  const { list, total, loading } = useSelector((state) => ({
+    ...state.countries,
+    loading: state.loadingBar.default,
+  }));
 
- useEffect(() => {
-  if (!list.length) {
-    dispatch(fetchCountries(continent));
+  useEffect(() => {
+    if (!list.length) {
+      dispatch(fetchCountries(continent));
+    }
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
   }
-}, []);
 
-if (loading) {
-  return <h1>Loading...</h1>;
-}
-
-return (
-  <section>
-    <h1>
-      {continent}
-      {' '}
-      -
-      {' '}
-      {total}
-    </h1>
-    <ul>
-      {list.map(({ country, confirmed }) => (
-        <li key={country}>
-          <Link to={`/country/${country}`}>
-            {country}
-            {' '}
-            -
-            {' '}
-            {confirmed}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </section>
-);
+  return (
+    <section>
+      <h1>
+        {continent}
+        {' '}
+        -
+        {' '}
+        {total}
+      </h1>
+      <ul>
+        {list.map(({ country, confirmed }) => (
+          <li key={country}>
+            <Link to={`/country/${country}`}>
+              {country}
+              {' '}
+              -
+              {' '}
+              {confirmed}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Home;
