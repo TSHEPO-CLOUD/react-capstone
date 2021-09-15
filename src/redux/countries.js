0d9c520fb18e4ec0320957ab9078dcfaba3dfa97
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-09-15 22:45:15 +0200

    Import api

--- src/redux/countries.js
+++ src/redux/countries.js
@@ -1,8 +1,10 @@
-const reducer = (state = {}, action) => {
- switch (action.type) {
-   default:
-     return state;
- }
-};
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import * as API from './api';
 
-export default reducer;
+const LOAD_COUNTRIES = 'cases/load-countries';
+const LOAD_COUNTRY = 'cases/load-country';
+
+export const loadCountries = (payload) => ({
+  type: LOAD_COUNTRIES,
+  payload,
+});
