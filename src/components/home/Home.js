d9152c87b72d7235ea052f7bdbf6a3ca59857f7d
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-09-16 11:47:37 +0200

    Import home.css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,6 +2,11 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchCountries } from '../../redux/countries';
+import Icon from '../Icon';
+import formatNumber from '../../utils/formatNumber';
+import './Home.css';
+import virus from '../../assets/virus.svg';
+import map from '../../assets/europe.png';
 
 const Home = () => {
   const continent = 'Europe';
